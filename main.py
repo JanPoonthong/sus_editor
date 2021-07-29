2511acd2fb823ae391dc1202db778a3b6e207f4e
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-30 00:35:06 +0700

    Change while loop logic

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sdl2
 import ctypes
 
+
 def main():
     sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
     window = sdl2.SDL_CreateWindow(
         b"Sus Editor", 0, 0, 800, 600, sdl2.SDL_WINDOW_RESIZABLE
     )
-    renderer = sdl2.SDL_CreateRenderer(window, -1, sdl2.SDL_RENDERER_ACCELERATED)
+    renderer = sdl2.SDL_CreateRenderer(
+        window, -1, sdl2.SDL_RENDERER_ACCELERATED
+    )
 
-    running = False
-    while not running:
+    running = True
+    while running:
         event = sdl2.SDL_Event()
         while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
             if event.type == sdl2.SDL_QUIT:
-                running = True
+                running = False
                 break
     sdl2.SDL_Quit()
-    return 0 
+    return 0
+
 
 main()
