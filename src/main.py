860bf296ea599bc74b74d9eaaaedf7aef9405d5b
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-30 17:07:46 +0700

    Implement stb to load an png

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
 from sdl2 import *
 import ctypes
+from ctypes import CDLL, byref
 from font import *
+import os
 
 
-def surface_from_file(filename):
-    w, h, n = 0, 0, 0
-    return 0
+def surface_from_file(file_path):
+    width, height, n, STBI_rgb_alpha = 0, 0, 0, 4
+    project_directory = os.path.dirname(os.path.abspath(__file__))
+    stb_image = CDLL(os.path.join(project_directory, "stb_image.so"))
+    pixels = stb_image.stbi_load(file_path, width, height, n, STBI_rgb_alpha)
+
+    if pixels == None:
+        print(
+            f"ERROR: couldn't load the file {file_path}: {stb_image.stbi_failure_reason()}"
+        )
+        exit(1)
+
+    rmask, gmask, bmask, amask = 0, 0, 0, 0
+    if SDL_BYTEORDER == SDL_BIG_ENDIAN:
+        rmask = 0xFF000000
+        gmask = 0x00FF0000
+        bmask = 0x0000FF00
+        amask = 0x000000FF
+    else:
+        rmask = 0x000000FF
+        gmask = 0x0000FF00
+        bmask = 0x00FF0000
+        amask = 0xFF000000
+
+    depth = 32
+    pitch = 4 * width
+    return SDL_CreateRGBSurfaceFrom(
+        pixels, width, height, depth, pitch, rmask, gmask, bmask, amask
+    )
 
 
 def main():
@@ -15,17 +43,7 @@ def main():
     )
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED)
 
-    # texture = SDL_CreateTexture(
-    #     renderer,
-    #     SDL_PIXELFORMAT_INDEX8,
-    #     SDL_TEXTUREACCESS_STATIC,
-    #     FONT_WIDTH,
-    #     FONT_HEIGHT
-    # )
-
-    surface = SDL_CreateRGBSurfaceFrom(FONT, FONT_WIDTH, FONT_HEIGHT, 8, FONT_WIDTH,
-                                      0xFF, 0xFF, 0xFF, 0xFF)
-
+    font_surface = surface_from_file("./charmap-oldschool_white.png")
     running = True
     while running:
         event = SDL_Event()
