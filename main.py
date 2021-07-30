89eb7595474d3bf30b001b167df1d2edaad98ed4
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-30 09:26:44 +0700

    Draw cyan color on to the 2d of the window

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import sdl2
+from sdl2 import *
 import ctypes
 
 
 def main():
-    sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
-    window = sdl2.SDL_CreateWindow(
-        b"Sus Editor", 0, 0, 800, 600, sdl2.SDL_WINDOW_RESIZABLE
+    SDL_Init(SDL_INIT_VIDEO)
+    window = SDL_CreateWindow(
+        b"Sus Editor", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE
     )
-    renderer = sdl2.SDL_CreateRenderer(
-        window, -1, sdl2.SDL_RENDERER_ACCELERATED
-    )
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED)
 
     running = True
     while running:
-        event = sdl2.SDL_Event()
-        while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
-            if event.type == sdl2.SDL_QUIT:
+        event = SDL_Event()
+        while SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == SDL_QUIT:
                 running = False
                 break
-    sdl2.SDL_Quit()
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 0)
+        SDL_RenderClear(renderer)
+        SDL_RenderPresent(renderer)
+    SDL_Quit()
     return 0
 
 
