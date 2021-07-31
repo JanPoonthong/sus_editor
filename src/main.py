2d88f081bbdf3024f1f21427d4a2a46c007c727a
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-31 14:41:49 +0700

    Render text as a bitmap font

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ import sys
 from sdl2 import *
 from sdl2.sdlimage import *
 
+FONT_WIDTH = 128
+FONT_HEIGHT = 64
+FONT_COLS = 18
+FONT_ROWS = 7
+FONT_CHAR_WIDTH = FONT_WIDTH / FONT_COLS
+FONT_CHAR_HEIGHT = FONT_HEIGHT / FONT_ROWS
+
 
 def scc(code):
     if code < 0:
@@ -32,8 +39,45 @@ def surface_from_file(file_path):
     return data.contents
 
 
+def render_char(renderer, font, c, x, y, color, scale):
+    index = c - 32
+    col = int(index % FONT_COLS)
+    row = int(index / FONT_COLS)
+
+    src = SDL_Rect(
+        x=int(col * FONT_CHAR_WIDTH),
+        y=int(row * FONT_CHAR_HEIGHT),
+        w=int(FONT_CHAR_WIDTH),
+        h=int(FONT_CHAR_HEIGHT),
+    )
+
+    # Output display
+    dst = SDL_Rect(
+        x=int(x),
+        y=int(y),
+        w=int(FONT_CHAR_WIDTH * scale),
+        h=int(FONT_CHAR_HEIGHT * scale),
+    )
+
+    SDL_SetTextureColorMod(
+        font,
+        color >> (8 * 2) & 0xFF,
+        color >> (8 * 1) & 0xFF,
+        color >> (8 * 0) & 0xFF,
+    )
+
+    scc(SDL_RenderCopy(renderer, font, src, dst))
+
+
+def renderer_text(renderer, font, text, x, y, color, scale):
+    for i in range(len(text)):
+        render_char(renderer, font, text[i], x, y, color, scale)
+        x += FONT_CHAR_WIDTH * scale
+    return 0
+
+
 def main():
-    SDL_Init(SDL_INIT_VIDEO)
+    scc(SDL_Init(SDL_INIT_VIDEO))
     window = SDL_CreateWindow(
         b"Sus Editor", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE
     )
@@ -41,22 +85,19 @@ def main():
 
     font_surface = surface_from_file(b"charmap-oldschool_white.png")
     font_texture = scp(SDL_CreateTextureFromSurface(renderer, font_surface))
-    font_rect = SDL_Rect(x=0, y=0, w=font_surface.w, h=font_surface.h)
 
     running = True
     while running:
-        event = SDL_Event()
+        event = scp(SDL_Event())
         while SDL_PollEvent(ctypes.byref(event)) != 0:
             if event.type == SDL_QUIT:
                 running = False
                 break
-        scc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0))
-        scc(SDL_RenderClear(renderer))
-        output_rect = SDL_Rect(
-            x=0, y=0, w=font_surface.w * 5, h=font_surface.h * 5
-        )
-        scc(SDL_RenderCopy(renderer, font_texture, font_rect, output_rect))
-        SDL_RenderPresent(renderer)
+            else:
+                renderer_text(
+                    renderer, font_texture, b"Jan Poonthong", 0, 0, 0x00FFFF, 5
+                )
+                SDL_RenderPresent(renderer)
     SDL_Quit()
     return 0
 
