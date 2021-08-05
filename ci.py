f580d88a3c17d2bafe002164d1091cac6a2df168
Jan Poonthong <janpoonthong628@gmail.com>
2021-08-05 10:52:46 +0700

    Add docs for ci.py and xvfb.py

--- ci.py
+++ ci.py
@@ -1,10 +1,12 @@
+"""DO NOT RUN IT FILE ON LOCAL MACHINE"""
 import subprocess
-subprocess.Popen('python3 main.py', shell=True)
+
+subprocess.Popen("python3 main.py", shell=True)
 from pynput.mouse import Button, Controller
 import time
 
 mouse = Controller()
-subprocess.Popen('xkill', shell=True)
+subprocess.Popen("xkill", shell=True)
 mouse.position = (200, 400)
 time.sleep(0.50)
 mouse.click(Button.left, 1)
--- xvfb.py
+++ xvfb.py
@@ -1,7 +1,11 @@
+"""DO NOT RUN IT FILE ON LOCAL MACHINE"""
 import subprocess
 import time
-subprocess.Popen('export DISPLAY=:1 && Xvfb "${DISPLAY}" -screen 0 1024x768x24', shell=True)
+
+subprocess.Popen(
+    'export DISPLAY=:1 && Xvfb "${DISPLAY}" -screen 0 1024x768x24', shell=True
+)
 time.sleep(1)
-subprocess.Popen('export DISPLAY=:1 && python3 ci.py', shell=True)
+subprocess.Popen("export DISPLAY=:1 && python3 ci.py", shell=True)
 time.sleep(1)
-subprocess.Popen('killall Xvfb', shell=True)
+subprocess.Popen("killall Xvfb", shell=True)
