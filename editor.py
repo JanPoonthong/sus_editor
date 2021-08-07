8d6545ba5cabbc4eb894e66719cc5865edd9a847
Jan Poonthong <janpoonthong628@gmail.com>
2021-08-07 13:14:10 +0700

    Trying to make editor instance work

--- editor.py
+++ editor.py
@@ -1,52 +1,60 @@
 class Line:
     chars = []
     size = 0
-    cursor = 0
 
 
 class Editor:
     lines = Line()
     size = 0
-    capacity = 0
     cursor_row = 0
     cursor_col = 0
 
 
+def editor_char_under_cursor(editor):
+    if editor.cursor_row < editor.size:
+        if editor.cursor_col < editor.lines.size:
+            return editor.lines.chars[editor.cursor_col]
+    return None
+
+
 def editor_insert_text_before(editor, text):
-    # if editor.cursor_row >= editor.size:
-    #     if editor.size > 0:
-    #         editor.cursor_row = editor.size - 1
-    #     else:
-    #         editor_push_new_line(editor)
+    if editor.cursor_row > editor.size:
+        if editor.size > 0:
+            editor.cursor_row = editor.size - 1
+        else:
+            editor_push_new_line(editor)
 
     line_insert_text_before(editor, text)
 
 
 def editor_backspace(editor):
-    # if editor.cursor_row >= editor.size:
-    #     if editor.size > 0:
-    #         editor.cursor_row = editor.size - 1
-    #     else:
-    #         editor_push_new_line(editor)
+    if editor.cursor_row > editor.size:
+        if editor.size > 0:
+            editor.cursor_row = editor.size - 1
+        else:
+            editor_push_new_line(editor)
+
     line_backspace(editor)
 
 
 def editor_delete(editor):
-    # if editor.cursor_row >= editor.size:
-    #     if editor.size > 0:
-    #         editor.cursor_row = editor.size - 1
-    #     else:
-    #         editor_push_new_line(editor)
+    if editor.cursor_row > editor.size:
+        if editor.size > 0:
+            editor.cursor_row = editor.size - 1
+        else:
+            editor_push_new_line(editor)
+
     line_delete(editor)
 
 
 def editor_push_new_line(editor):
-    pass
+    editor.size += 1
+    editor.cursor_row += 1
 
 
 def line_insert_text_before(editor, text):
-    # if col > line.size:
-    #     col = line.size
+    if editor.cursor_col > editor.lines.size:
+        editor.cursor_col = editor.lines.size
 
     text_size = len(text)
     editor.lines.chars.insert(editor.cursor_col, text)
@@ -55,8 +63,8 @@ def line_insert_text_before(editor, text):
 
 
 def line_backspace(editor):
-    # if col > line.size:
-    #     col = line.size
+    if editor.cursor_col > editor.lines.size:
+        editor.cursor_col = editor.lines.size
 
     if editor.cursor_col > 0 and editor.lines.size > 0:
         editor.cursor_col -= 1
