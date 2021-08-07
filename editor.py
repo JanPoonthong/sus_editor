4ea63429380a1ce713343c2592df29418b7afdb4
Ciremun <Ciremun@yandex.ru>
2021-08-07 16:21:08 +0300

    fixes new line insertion

--- editor.py
+++ editor.py
@@ -1,19 +1,20 @@
 class Line:
-    chars = []
-    size = 0
+    def __init__(self):
+        self.chars = []
+        self.size = 0
 
 
 class Editor:
-    lines = Line()
+    lines = [Line()]
     size = 1
     cursor_row = 0
     cursor_col = 0
-
+    current_line = 0
 
 def editor_char_under_cursor(editor):
     if editor.cursor_row < editor.size:
-        if editor.cursor_col < editor.lines.size:
-            return editor.lines.chars[editor.cursor_col]
+        if editor.cursor_col < editor.lines[editor.current_line].size:
+            return editor.lines[editor.current_line].chars[editor.cursor_col]
     return None
 
 
@@ -51,32 +52,34 @@ def editor_push_new_line(editor):
     editor.size += 1
     editor.cursor_row += 1
     editor.cursor_col = editor.size - editor.size
+    editor.lines.append(Line())
+    editor.current_line += 1
 
 
 def line_insert_text_before(editor, text):
-    if editor.cursor_col > editor.lines.size:
-        editor.cursor_col = editor.lines.size
+    if editor.cursor_col > editor.lines[editor.current_line].size:
+        editor.cursor_col = editor.lines[editor.current_line].size
 
     text_size = len(text)
-    editor.lines.chars.insert(editor.cursor_col, text)
-    editor.lines.size += text_size
+    editor.lines[editor.current_line].chars.insert(editor.cursor_col, text)
+    editor.lines[editor.current_line].size += text_size
     editor.cursor_col += text_size
 
 
 def line_backspace(editor):
-    if editor.cursor_col > editor.lines.size:
-        editor.cursor_col = editor.lines.size
+    if editor.cursor_col > editor.lines[editor.current_line].size:
+        editor.cursor_col = editor.lines[editor.current_line].size
 
-    if editor.cursor_col > 0 and editor.lines.size > 0:
+    if editor.cursor_col > 0 and editor.lines[editor.current_line].size > 0:
         editor.cursor_col -= 1
-        editor.lines.size -= 1
-        del editor.lines.chars[editor.cursor_col]
+        editor.lines[editor.current_line].size -= 1
+        del editor.lines[editor.current_line].chars[editor.cursor_col]
 
 
 def line_delete(editor):
-    if editor.cursor_col > editor.lines.size:
-        editor.cursor_col = editor.lines.size
+    if editor.cursor_col > editor.lines[editor.current_line].size:
+        editor.cursor_col = editor.lines[editor.current_line].size
 
-    if 0 <= editor.cursor_col < editor.lines.size:
-        editor.lines.size -= 1
-        del editor.lines.chars[editor.cursor_col]
+    if 0 <= editor.cursor_col < editor.lines[editor.current_line].size:
+        editor.lines[editor.current_line].size -= 1
+        del editor.lines[editor.current_line].chars[editor.cursor_col]
